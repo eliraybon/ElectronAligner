d26d721c1567f40e5bfc141b70cd46ec4365273c
eliraybon <elr0615@gmail.com>
2019-12-30 16:38:56 -0500

    Add basic Kick synth engine

---
+++ src/synthesis/Kick.js
@@ -0,0 +1,27 @@
+export default class Kick {
+  constructor(context) {
+    this.context = context;
+  }
+
+  setup = () => {
+    this.osc = this.context.createOscillator();
+    this.gain = this.context.createGain();
+    this.osc.connect(this.gain);
+    this.gain.connect(this.context.destination)
+  }
+
+  //should you try setting up in the constructor to avoid repeat setup?
+  trigger = time => {
+    this.setup();
+
+    this.osc.frequency.setValueAtTime(150, time);
+    this.gain.gain.setValueAtTime(1, time);
+
+    this.osc.frequency.exponentialRampToValueAtTime(0.01, time + 0.5);
+    this.gain.gain.exponentialRampToValueAtTime(0.01, time + 0.5);
+
+    this.osc.start(time);
+
+    this.osc.stop(time + 0.5);
+  }
+}
