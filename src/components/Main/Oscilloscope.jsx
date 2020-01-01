172a0ac1117c8aeef589f1e11002e84367c29def
eliraybon <elr0615@gmail.com>
2019-12-31 23:17:33 -0500

    Implement frequency bar visualization

--- src/components/Main/Oscilloscope.jsx
+++ src/components/Main/Oscilloscope.jsx
@@ -4,6 +4,9 @@ export default class Oscilloscope extends React.Component {
   constructor(props) {
     super(props);
     const { context, analyser } = this.props;
+    this.state = {
+      mode: 'bar' //two modes: 'osc' and 'bar'
+    }
 
     document.addEventListener("DOMContentLoaded", () => {
       this.canvas = document.querySelector('.osc-canvas');
@@ -26,7 +29,7 @@ export default class Oscilloscope extends React.Component {
 
   visualize = () => {
     this.canvasCtx.clearRect(0, 0, this.WIDTH, this.HEIGHT);
-    this.draw();
+    (this.state.mode === 'osc') ? this.draw() : this.drawBars();
   }
 
   draw = () => {
@@ -63,6 +66,32 @@ export default class Oscilloscope extends React.Component {
 
     this.canvasCtx.lineTo(this.canvas.width, this.canvas.height / 2);
     this.canvasCtx.stroke();
+  }
+
+  drawBars = () => {
+    this.analyser.fftSize = 256;
+    let bufferLengthAlt = this.analyser.frequencyBinCount;
+    const dataArrayAlt = new Uint8Array(bufferLengthAlt);
+
+    const drawVisual = requestAnimationFrame(this.drawBars);
+
+    this.analyser.getByteFrequencyData(dataArrayAlt);
+
+    this.canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+    this.canvasCtx.fillRect(0, 0, this.WIDTH, this.HEIGHT);
+
+    let barWidth = (this.WIDTH / bufferLengthAlt) * 2.5;
+    let barHeight;
+    let x = 0;
+
+    for (let i = 0; i < bufferLengthAlt; i++) {
+      barHeight = dataArrayAlt[i];
+
+      this.canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+      this.canvasCtx.fillRect(x, this.HEIGHT - barHeight / 2, barWidth, barHeight / 2);
+
+      x += barWidth + 1;
+    }
   }
 
   render() {
