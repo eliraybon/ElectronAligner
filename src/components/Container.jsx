91fb1083867d2d4790323f4afd7976dc5c8e5f07
eliraybon <elr0615@gmail.com>
2020-01-03 17:10:09 -0500

    Add instructions

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -27,8 +27,16 @@ export default class Container extends React.Component {
         {this.state.instructions && (
           <div className="instructions-background" onClick={this.toggleInstructions}>
             <div className="instructions">
-              <button><i className="fas fa-times fa-2x"></i></button>
-              <h2>Instructions</h2>
+              <button><i className="fas fa-times"></i></button>
+              <h2 className="instructions-header">Instructions</h2>
+              <ul className="bullet-points">
+                <li>Click the buttons in the grid to activate steps in the sequencer</li>
+                <li>Press the Space Bar or Play Button to play and pause your sequence</li>
+                <li>Press Enter the jump to the beginning of the sequence</li>
+                <li>Use the up and down arrow keys to adjust the tempo</li>
+                <li>Adjust the sliders in the top right to change the sounds live</li>
+                <li>Activate the wildcard buttons in the middle row for unexpected effects</li>
+              </ul>
             </div>
           </div>
         )}
