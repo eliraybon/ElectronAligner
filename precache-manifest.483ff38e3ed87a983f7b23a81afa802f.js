656f71f4a7d015b003d3f095327553f51e14dc15
eliraybon <elr0615@gmail.com>
2020-01-03 22:05:15 -0500

    Updates

---
+++ precache-manifest.483ff38e3ed87a983f7b23a81afa802f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7eba591dfd209513449a1417b7e07c1",
+    "url": "/ElectronAligner/index.html"
+  },
+  {
+    "revision": "48fd9311a15ec1e59fb3",
+    "url": "/ElectronAligner/static/js/2.d7310e92.chunk.js"
+  },
+  {
+    "revision": "8434f97e4d2f72ca2061a170f38d5223",
+    "url": "/ElectronAligner/static/js/2.d7310e92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3e03e95585c2d9f7e21",
+    "url": "/ElectronAligner/static/js/main.60641817.chunk.js"
+  },
+  {
+    "revision": "e5da0e7aa9fd309a104d",
+    "url": "/ElectronAligner/static/js/runtime-main.5b75897f.js"
+  }
+]);
